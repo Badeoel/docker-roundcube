38d8593ffded9bbacc71b690d255a66932b3e4a4
Badeoel <caroli@visual-design.com>
2016-08-29 12:29:43 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -20,6 +20,11 @@ $config['smtp_port'] = 587;
 $config['smtp_user'] = '%u';
 $config['smtp_pass'] = '%p';
 
-$config['plugins'] = array('carddav', 'plugin-manager', 'managesieve', 'enigma','keyboard_shortcuts', 'archive', 'contextmenu', 'emoticons', 'vcard_attachments');
+$config['plugins'] = array('carddav', 'filters', 'enigma','keyboard_shortcuts', 'archive', 'contextmenu', 'emoticons', 'vcard_attachments');
+  
+$config['autoAddSpamFilterRule'] = TRUE;  // if TRUE a spam filter rule is created for all users which automatically move messages into junk folder  
+$config['spam_subject'] = '[SPAM]';       // How to mark the spam in the subject? To have effect the previous variable must be TRUE.
+$config['caseInsensitiveSearch'] = TRUE;  // if TRUE filters searching in case insensitive mode.
+$config['decodeBase64Msg'] = TRUE;        // if TRUE decodes base64 mail messages.
 
 if(getenv('ROUNDCUBE_USER_FILE')) $config['plugins'][] = 'password';
