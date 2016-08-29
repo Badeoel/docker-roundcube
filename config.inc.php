15fb5afcdb2b403eb2bcaabb0e20279fe1fe7636
Badeoel <caroli@visual-design.com>
2016-08-29 13:29:27 +0200

    Other plugins activated

--- config.inc.php
+++ config.inc.php
@@ -20,7 +20,7 @@ $config['smtp_port'] = 587;
 $config['smtp_user'] = '%u';
 $config['smtp_pass'] = '%p';
 
-$config['plugins'] = array('carddav', 'filters', 'enigma','keyboard_shortcuts', 'archive', 'contextmenu', 'emoticons', 'vcard_attachments');
+$config['plugins'] = array('carddav', 'enigma','keyboard_shortcuts', 'archive', 'contextmenu', 'emoticons', 'vcard_attachments', 'maskasjunk', 'help','debug_logger', 'customizr');
   
 $config['autoAddSpamFilterRule'] = TRUE;  // if TRUE a spam filter rule is created for all users which automatically move messages into junk folder  
 $config['spam_subject'] = '[SPAM]';       // How to mark the spam in the subject? To have effect the previous variable must be TRUE.
